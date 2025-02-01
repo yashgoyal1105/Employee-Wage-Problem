11908da8099b4af04ac71240b90ce58b87cadf3f
yashgoyal1122 <yg9532@srmist.edu.in>
2025-02-01 14:32:14 +0530

    [Yash Goyal] add: python script for calculating daily wage of an employee

--- emp.py
+++ emp.py
@@ -1,1 +1,29 @@
-print("Welcome to employee wage computation")
+import random
+
+PER_HOUR_WAGE = 20
+FULL_DAY_HOURS = 8
+
+class EmployeeWage:
+
+    @staticmethod
+    def check_attendance():
+        attendance = random.randint(0, 1)
+        if attendance == 1:
+            return "Present"
+        else:
+            return "Absent"
+        
+    def calculate_daily_wage():
+        attendance_status = EmployeeWage.check_attendance()
+        if attendance_status == "Present":
+            daily_wage = PER_HOUR_WAGE * FULL_DAY_HOURS
+        else:
+            daily_wage = 0
+        return daily_wage, attendance_status
+
+print("Welcome to Employee Wage Computation Program")
+
+daily_wage, attendance_status = EmployeeWage.calculate_daily_wage()
+
+print(f"Employee is {attendance_status}")
+print(f"Daily Wage: ₹{daily_wage}")
