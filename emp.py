080fa5fb6490c65b43b9c5994ca0a8371f74c36c
yashgoyal1122 <yg9532@srmist.edu.in>
2025-02-01 10:53:57 +0530

    [Yash Goyal] add: python script to check attendance of an employee

--- emp.py
+++ emp.py
@@ -1,1 +1,13 @@
-print("Welcome to employee wage computation")
+import random
+
+def check_attendance():
+    print("Welcome to Employee Wages computation program in Master Branch")
+    status = random.choice([1,0])
+    if status == 0:
+        return "Absent"
+    else:
+        return "Present"
+    
+attendance_status = check_attendance()
+print(f"Employee is {attendance_status}")
+print(f"Welcome to Employee wage computation")
